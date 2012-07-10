ebc544303755560c9fdb810541fcf22985bced49
Patrick Cushing + John Davidson <apprentice+pc+jd@devbootcamp.com>
2012-07-09 20:09:11 -0700

    Creates a library of ruby classes and methods

---
+++ lib/library.rb
@@ -0,0 +1,27 @@
+require 'nokogiri'
+require 'open-uri'
+
+module Reflector
+  class Library
+    attr_reader :raw_methods, :library, :classes
+
+    def initialize(url)
+      @class_doc = Nokogiri::HTML(open(url))
+      @library = make_library
+      @classes = @library.collect {|key, value| key}
+    end
+
+    def make_library
+      library_hash = {}
+      @raw_methods = @class_doc.css('div.entries a').collect{ |line| line.content.to_s } #this can be updated with John's new query
+      @raw_methods.each do |line|
+        unless line.split(' ')[1].nil? #this can go once we updated @raw_methods to ignore the nils
+          method = line.split(' ')[0]
+          class_name = line.split(' ')[1].gsub(/\(|\)/,'')
+          !library_hash[class_name].nil? ? (library_hash[class_name] << method) : (library_hash[class_name] = [method])
+        end
+      end
+      library_hash
+    end
+  end
+end
---
+++ spec/library_spec.rb
@@ -0,0 +1,32 @@
+require 'rspec'
+require '../lib/library.rb'
+
+describe Reflector::Library do
+
+  it "will not initialize without the target library" do
+    expect { doc = Reflector::Library.new }.should raise_error
+  end
+
+  before :all do
+    @class_doc = Reflector::Library.new('http://ruby-doc.org/core-1.9.3/')
+  end
+
+  it "initializes a new library" do
+    @class_doc.should be_an_instance_of Reflector::Library
+  end
+
+  it "selects all of the methods from ruby doc" do
+    @class_doc.raw_methods.length.should be > 1700
+  end
+
+  it "returns a library hash with class keys and arrays of their respective methods" do
+    @class_doc.library["SystemCallError"].should eq ["===", "new", "errno"]
+    @class_doc.library["SignalException"].should eq ["new", "signo"]
+  end
+
+  it "returns just the classes from the library" do
+    @class_doc.classes.length.should eq 68
+  end
+end
+
+
