a521cc0e3fe6de9b5db67857907f124cd33e6c1e
Robert Fletcher <lobatifricha@gmail.com>
2012-07-15 22:42:22 -0700

    remove spec_suite.rb

--- spec/spec_suite.rb
+++
@@ -1,9 +0,0 @@
-require 'simplecov'
-SimpleCov.start
-
-require './dir_parser_spec.rb'
-require './file_parser_spec.rb'
-require './database_spec.rb'
-require './library_spec.rb'
-require './project_stats_spec.rb'
-require './repo_retrieve_spec.rb'
