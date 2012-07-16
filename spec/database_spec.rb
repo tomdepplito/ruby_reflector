8d5d2ca37a1057ecfb637b8c3f5fd89cafe6f9f3
Robert Fletcher <lobatifricha@gmail.com>
2012-07-15 22:33:30 -0700

    re-indenting database_spec.rb

--- spec/database_spec.rb
+++ spec/database_spec.rb
@@ -3,66 +3,64 @@ require 'spec_helper'
 include Reflector
 
 describe Database do
-    def database_path
-      File.join(root_path, 'db', 'test.db')
-    end
+  def database_path
+    File.join(root_path, 'db', 'test.db')
+  end
 
-    before :each do
-      @database = Reflector::Database.new(database_path)
-      @db = SQLite3::Database.new(database_path)
-    end
+  before :each do
+    @database = Reflector::Database.new(database_path)
+    @db = SQLite3::Database.new(database_path)
+  end
 
-    after :all do
-      File.delete(database_path)
-    end
+  after :all do
+    File.delete(database_path)
+  end
 
-    it "the database exists" do
-      File.exists?( database_path ).should be_true
-    end
+  it "the database exists" do
+    File.exists?( database_path ).should be_true
+  end
 
-    it "writes the methods library to the db" do
-      @db.execute("SELECT * FROM methods").length.should eq 850
-    end
+  it "writes the methods library to the db" do
+    @db.execute("SELECT * FROM methods").length.should eq 850
+  end
 
-    it "has 3 tables and the sqlite sequence table" do
-      @db.execute("SELECT name FROM sqlite_master WHERE type = 'table'").length.should eq 4
-    end
+  it "has 3 tables and the sqlite sequence table" do
+    @db.execute("SELECT name FROM sqlite_master WHERE type = 'table'").length.should eq 4
+  end
 
-    it "writes a project to the repos table" do
-      @database.repos_write({
-          :url => "https://github.com/Devbootcamp/RR_RnR",
-          :clone_url => "https://github.com/Devbootcamp/RR_RnR.git",
-          :last_commit_date => "NULL",
-          :name => "RR_RnR",
-          :methods => { :length => 3, :== => 2, :include? => 1, :each => 1, :map => 1 }
-      })
-      @db.execute("SELECT * FROM repos").first[0].should eq 1
-      @db.execute("SELECT * FROM repos").first[1].should eq "RR_RnR"
-      @db.execute("SELECT * FROM repos").first[2].should eq "https://github.com/Devbootcamp/RR_RnR"
-      @db.execute("SELECT * FROM repos").first[3].should eq "https://github.com/Devbootcamp/RR_RnR.git"
-      @db.execute("SELECT * FROM repos").first[4].should eq "NULL"
-    end
+  it "writes a project to the repos table" do
+    @database.repos_write({
+        :url => "https://github.com/Devbootcamp/RR_RnR",
+        :clone_url => "https://github.com/Devbootcamp/RR_RnR.git",
+        :last_commit_date => "NULL",
+        :name => "RR_RnR",
+        :methods => { :length => 3, :== => 2, :include? => 1, :each => 1, :map => 1 }
+    })
+    @db.execute("SELECT * FROM repos").first[0].should eq 1
+    @db.execute("SELECT * FROM repos").first[1].should eq "RR_RnR"
+    @db.execute("SELECT * FROM repos").first[2].should eq "https://github.com/Devbootcamp/RR_RnR"
+    @db.execute("SELECT * FROM repos").first[3].should eq "https://github.com/Devbootcamp/RR_RnR.git"
+    @db.execute("SELECT * FROM repos").first[4].should eq "NULL"
+  end
 
-    it "return result set from the repos table" do
-      repos_rows = @db.execute ("SELECT COUNT(*) FROM repos")
-      rs = @database.repos_get
-      rs.count.should eq repos_rows[0][0]
-    end
+  it "return result set from the repos table" do
+    repos_rows = @db.execute ("SELECT COUNT(*) FROM repos")
+    rs = @database.repos_get
+    rs.count.should eq repos_rows[0][0]
+  end
 
-    it "writes the repo and its methods to the db" do
-      @database.repos_write({
-          :url => "https://github.com/Devbootcamp/RR_RnR",
-          :clone_url => "https://github.com/Devbootcamp/RR_RnR.git",
-          :last_commit_date => "NULL",
-          :name => "RR_RnR",
-          :methods => { :length => 3, :== => 2, :include? => 1, :each => 1, :map => 1 }
-      })
-      @db.execute("SELECT methods_stats.count FROM methods_stats INNER JOIN methods ON methods.id = methods_stats.method_id WHERE methods.name like 'length'")[0][0].should eq 3
-    end
+  it "writes the repo and its methods to the db" do
+    @database.repos_write({
+        :url => "https://github.com/Devbootcamp/RR_RnR",
+        :clone_url => "https://github.com/Devbootcamp/RR_RnR.git",
+        :last_commit_date => "NULL",
+        :name => "RR_RnR",
+        :methods => { :length => 3, :== => 2, :include? => 1, :each => 1, :map => 1 }
+    })
+    @db.execute("SELECT methods_stats.count FROM methods_stats INNER JOIN methods ON methods.id = methods_stats.method_id WHERE methods.name like 'length'")[0][0].should eq 3
+  end
 
-    it "reads the methods from the db based on a repo name" do
-      @database.methods_stats_read("RR_RnR").length.should eq 10
-    end
+  it "reads the methods from the db based on a repo name" do
+    @database.methods_stats_read("RR_RnR").length.should eq 10
+  end
 end
-
-
