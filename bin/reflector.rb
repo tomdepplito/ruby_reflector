0112d177965875a49c3c5057f8991ff5e1667c07
Robert Fletcher <lobatifricha@gmail.com>
2012-07-17 00:48:59 -0700

    remove @ from variables

--- bin/reflector.rb
+++ bin/reflector.rb
@@ -5,41 +5,36 @@ Dir.glob(File.join(ROOT_PATH, 'lib', '**', '*.rb')).each do |file|
   require file
 end
 
-@url = ARGV[0]
+url = ARGV[0]
 
-@repository = Reflector::RepoRetriever.new(@url)
-@db = Reflector::Database.new('../db/reflector.db')
+repository = Reflector::RepoRetriever.new(url)
+db = Reflector::Database.new('../db/reflector.db')
 
 
-@files = Reflector::DirParser.new("../repos/" + @repository.repository_name).files # Gives us all our files
+files = Reflector::DirParser.new("../repos/" + repository.repository_name).files # Gives us all our files
 
 # parses each file
 # does the lookup to the library
 # instantiates a new method_stats class per file
-methods = @files.collect { |file|  puts file; Reflector::Parser.new(file).method_array }
+methods = files.collect { |file|  puts file; Reflector::Parser.new(file).method_array }
 
 puts "Constructing repo stats"
-project_stats = Reflector::ProjectStats.new(@repository.repository_name) #needs to be created: url and commit_date
+project_stats = Reflector::ProjectStats.new(repository.repository_name) #needs to be created: url and commit_date
 
 project_stats.inc_stats(methods)
 
 puts project_stats.stats_results
 
-@db.repos_write({
+db.repos_write({
     :url => "https://github.com/Devbootcamp/RR_RnR",
-    :clone_url => @repository.clone_url,
+    :clone_url => repository.clone_url,
     :last_commit_date => "NULL",
-    :name => @repository.repository_name,
+    :name => repository.repository_name,
     :methods => project_stats.stats_results
 })
 
-# puts @db.method_stats_read(@repository.repository_name)
+presentation = Reflector::Presentation.new(db.methods_stats_read(repository.repository_name), repository.repository_name)
+puts presentation.console_print
 
-puts "Holy shit.  It might have worked.  Check the ../db/reflector.db"
-
-
-@presentation = Reflector::Presentation.new(@db.methods_stats_read(@repository.repository_name), @repository.repository_name)
-puts @presentation.console_print
-
-@repository.delete_repository
+repository.delete_repository
 
